6e599c002822be3d52e22fcdaf5c2d4a301d629c
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 12:56:27 -0300

    fix: use Nexus application id

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const apiBaseUrl = "https://api.petertecnet.com.br/api";
 const storageUrl = "https://api.petertecnet.com.br/storage/";
 
-const appId =  4;
-const linkApp ="https://nexus.petertecnet.com.br";
-const logoApp ="https://nexus.petertecnet.com.br/images/logo.png";
+const appId = 2;
+const linkApp = "https://nexus.petertecnet.com.br";
+const logoApp = "https://nexus.petertecnet.com.br/images/logo.png";
+
 export { apiBaseUrl, storageUrl, appId, linkApp, logoApp };
