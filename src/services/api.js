806b2f0dbe0922762b77b45549f89289a853cf7f
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 15:12:56 -0300

    refactor: centralize API auth and timeout handling

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,33 @@ import { apiBaseUrl } from "../config";
 
 const api = axios.create({
   baseURL: apiBaseUrl,
+  timeout: 20000,
+  headers: {
+    Accept: "application/json",
+  },
 });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token && !config.headers?.Authorization) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error?.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.dispatchEvent(new Event("authChanged"));
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
