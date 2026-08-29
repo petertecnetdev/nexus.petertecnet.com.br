f97a4ed1839b74ad4f4375079bdfdaa5c828a548
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 21:21:46 -0300

    feat: show Peter Tecnet signature globally

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import PeterTecnetSignature from "./components/PeterTecnetSignature";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <PeterTecnetSignature />
   </React.StrictMode>
 );
