bb9c988bda4323afebafdc291b8a1a35c42d543c
Peter Tecnet <petertecnet@gmail.com>
2026-08-29 14:44:39 -0300

    Sign direct Nexus axios requests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import axios from "axios";
 import App from "./App";
 import PeterTecnetSignature from "./components/PeterTecnetSignature";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+
+axios.defaults.headers.common["X-Peter-App"] = "nexus";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
