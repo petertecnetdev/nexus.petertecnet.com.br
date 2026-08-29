995c57563198e3d7c956dfc6d1fce2a278abf37a
Peter Tecnet <petertecnet@gmail.com>
2026-08-29 14:43:51 -0300

    Identify Nexus in every API request

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,23 @@
 import axios from "axios";
 import { apiBaseUrl } from "../config";
 
+const APP_SLUG = "nexus";
+
 const api = axios.create({
   baseURL: apiBaseUrl,
   timeout: 20000,
   headers: {
     Accept: "application/json",
+    "X-Peter-App": APP_SLUG,
   },
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+  config.headers = config.headers || {};
+  config.headers["X-Peter-App"] = APP_SLUG;
 
-  if (token && !config.headers?.Authorization) {
-    config.headers = config.headers || {};
+  if (token && !config.headers.Authorization) {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
@@ -27,7 +31,6 @@ api.interceptors.response.use(
       localStorage.removeItem("token");
       window.dispatchEvent(new Event("authChanged"));
     }
-
     return Promise.reject(error);
   }
 );
