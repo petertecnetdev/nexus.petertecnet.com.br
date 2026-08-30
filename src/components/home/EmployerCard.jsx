7302ae2136c0717f02403c8bd41b72a8cc0826fc
Peter Tecnet <petertecnet@gmail.com>
2026-08-30 17:05:24 -0300

    fix: use initials in legacy employer card

--- src/components/home/EmployerCard.jsx
+++ src/components/home/EmployerCard.jsx
@@ -1,16 +1,15 @@
 // src/components/home/EmployerCard.jsx
 import React from "react";
+import PropTypes from "prop-types";
+import EntityImage from "../EntityImage";
 import "./EmployerCard.css";
 
 export default function EmployerCard({ data, openSchedulePopup }) {
-  const img =
-    data?.avatar ||
-    data?.images?.avatar ||
-    "/images/placeholder.png";
+  const name = data?.name || "Colaborador";
+  const images = [data?.avatar, data?.images?.avatar, data?.user?.avatar];
 
   const handleScheduleClick = () => {
     if (typeof openSchedulePopup !== "function") return;
-
     openSchedulePopup({
       type: "employer",
       id: data.id,
@@ -21,17 +20,18 @@ export default function EmployerCard({ data, openSchedulePopup }) {
   return (
     <div className="ecard" onClick={handleScheduleClick} style={{ cursor: "pointer" }}>
       <div className="ecard-top">
-        <img src={img} alt={data.name} className="ecard-avatar" />
-
+        <EntityImage src={images} name={name} alt={name} shape="round" className="ecard-avatar" loading="lazy" />
         <div className="ecard-info">
-          <div className="ecard-name">{data.name}</div>
+          <div className="ecard-name">{name}</div>
           <div className="ecard-sub">{data.establishment?.name}</div>
         </div>
       </div>
-
-      <div className="ecard-stats">
-        {data.completed_appointments} atendimentos concluídos
-      </div>
+      <div className="ecard-stats">{data.completed_appointments} atendimentos concluídos</div>
     </div>
   );
 }
+
+EmployerCard.propTypes = {
+  data: PropTypes.object.isRequired,
+  openSchedulePopup: PropTypes.func,
+};
