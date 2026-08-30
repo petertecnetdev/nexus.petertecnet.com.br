3832c9a1bc6236f0691eb70aec6f9db38b33e709
Peter Tecnet <petertecnet@gmail.com>
2026-08-29 21:58:12 -0300

    feat: start interaction telemetry for every session

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { startTelemetry } from "./telemetry";
+import { apiBaseUrl, appSlug } from "./config";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import axios from "axios";
@@ -5,6 +7,8 @@ import App from "./App";
 import PeterTecnetSignature from "./components/PeterTecnetSignature";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+
+startTelemetry({ apiBaseUrl, appSlug });
 
 axios.defaults.headers.common["X-Peter-App"] = "nexus";
 
