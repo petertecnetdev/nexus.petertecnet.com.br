fadf49e65b94517b873d68fd487147886dfb1b18
Peter Tecnet <petertecnet@gmail.com>
2026-09-05 13:39:43 -0300

    feat: enable WhatsApp fallback on every view

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { startTelemetry } from "./telemetry";
 import { apiBaseUrl, appId, appSlug } from "./config";
 import { installGlobalImageFallbacks } from "./utils/imageFallback";
 import { installPasswordFieldEnhancer } from "./utils/passwordFieldEnhancer";
+import { installPeterWhatsappFallback } from "./utils/peterWhatsappFallback";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import axios from "axios";
@@ -14,6 +15,7 @@ import "./styles/nexus-brand-refresh.css";
 
 installGlobalImageFallbacks();
 installPasswordFieldEnhancer();
+installPeterWhatsappFallback();
 startTelemetry({ apiBaseUrl, appSlug, appId });
 
 axios.defaults.headers.common["X-Peter-App"] = appSlug;
