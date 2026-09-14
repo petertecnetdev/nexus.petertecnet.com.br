baa49e3eb3cabb0f084c92fd32d19869127bdecd
Peter Tecnet <petertecnet@gmail.com>
2026-09-14 11:41:29 -0300

    refactor(commerce): centralize terminal payment states

--- src/pages/commerce/paymentRecovery.js
+++ src/pages/commerce/paymentRecovery.js
@@ -5,6 +5,11 @@ export const RETRYABLE_PAYMENT_STATUSES = new Set([
   "canceled",
 ]);
 
+export const TERMINAL_PAYMENT_STATUSES = new Set([
+  "refunded",
+  ...RETRYABLE_PAYMENT_STATUSES,
+]);
+
 export const CONFIRMED_PAYMENT_STATUSES = new Set([
   "paid",
   "approved",
@@ -15,6 +20,9 @@ export const normalizePaymentStatus = (status) =>
 
 export const isRetryablePaymentStatus = (status) =>
   RETRYABLE_PAYMENT_STATUSES.has(normalizePaymentStatus(status));
+
+export const isTerminalPaymentStatus = (status) =>
+  TERMINAL_PAYMENT_STATUSES.has(normalizePaymentStatus(status));
 
 export const isConfirmedPaymentStatus = (status) =>
   CONFIRMED_PAYMENT_STATUSES.has(normalizePaymentStatus(status));
